6d052c1717537fcb7f1313b7936f410033e025e8
sun-nyday <114508094+sun-nyday@users.noreply.github.com>
2022-10-25 11:59:18 -0400

    Add files via upload

---
+++ explore.py
@@ -0,0 +1,40 @@
+import random
+
+#first 3 days, visit each cafeteria
+def exploitOnly():
+    pass
+
+# random libray -> random.normalvariate(mean, deviation)
+# happiness  = random.normalvariate(H1, D1)
+# visit each cafeteria 100 times
+# C1; 100
+# C2: 100
+# C3: 100
+# Each day calculate happiness based on normal distribution w/certain mean and standard deviation
+# return the total sum of happiness value
+
+# average = 100*H1 + 100*H2+100*3
+# regret = 3000-average
+def exploreOnly():
+    pass
+# countC1 = 0
+# happiness (for all 300 days?) = 0
+# while countC1 < 100:
+    # generate a happiness value based on the cafeteria
+    # i = random.normalvariate(H1, D1)
+    # happiness = happiness + i
+    # countC1 = countC1 + 1 / countC1 += 1
+# return happiness
+
+
+# take input value of e (percent value)
+def eGreedy(e=10):
+    pass
+
+# optimum happiness
+# expected happiness
+# expected regret
+# run simulation, for t trials
+# compare them to the expected values
+def simu():
+    pass
