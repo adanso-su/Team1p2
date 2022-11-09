e98320841c39cab6bf4977e1d3c7c5ac09f81fec
YuchaoBian <114174257+YuchaoBian@users.noreply.github.com>
2022-11-09 11:35:43 -0500

    Update eGreedy.py

--- eGreedy.py
+++ eGreedy.py
@@ -11,31 +11,46 @@ def eGreedy(e=10):
         exit(1)
     H = [H1, H2, H3]
     D = [D1, D2, D3]
+    H1List = []
+    H2List = []
+    H3List = []
     Happiness1 = random.normalvariate(H1, D1)
     Happiness2 = random.normalvariate(H2, D2)
     Happiness3 = random.normalvariate(H3, D3)
     Happiness = [Happiness1, Happiness2, Happiness3]
     maxIndex = Happiness.index(max(Happiness))
-    maxHappiness = Happiness[maxIndex]
     sumHappiness = Happiness1 + Happiness2 + Happiness3
+    H1List.append(Happiness1)
+    H2List.append(Happiness2)
+    H3List.append(Happiness3)
     for i in range(297):
         r = random.random()
         if r < e / 100:
             selectCafeterias = random.randint(0, 2)
             if selectCafeterias == 0:
                 timeHappiness = random.normalvariate(H1, D1)
+                H1List.append(timeHappiness)
             elif selectCafeterias == 1:
                 timeHappiness = random.normalvariate(H2, D2)
+                H2List.append(timeHappiness)
             else:
                 timeHappiness = random.normalvariate(H3, D3)
-            if timeHappiness > maxHappiness:
-                maxHappiness = timeHappiness
-                maxIndex = selectCafeterias
+                H3List.append(timeHappiness)
         else:
             h = H[maxIndex]
             d = D[maxIndex]
             timeHappiness = random.normalvariate(h, d)
-            maxHappiness = max(timeHappiness, maxHappiness)
+            if maxIndex == 0:
+                H1List.append(timeHappiness)
+            elif maxIndex == 1:
+                H2List.append(timeHappiness)
+            else:
+                H3List.append(timeHappiness)
+        average1 = sum(H1List) / len(H1List)
+        average2 = sum(H2List) / len(H2List)
+        average3 = sum(H3List) / len(H3List)
+        averageList = [average1, average2, average3]
+        maxIndex = averageList.index(max(averageList))
         sumHappiness += timeHappiness
     return sumHappiness
 print(eGreedy(10))
